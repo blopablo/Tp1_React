b5cc5fb42f0679bb95fe2d159f3636128ca586a3
blopablo <olbap0991@gmail.com>
2025-11-01 15:15:17 -0300

    error de sintaxis en App a app

--- Info-store/src/main.jsx
+++ Info-store/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // importa el App verdadero
+import App from "./app.jsx"; // importa el App verdadero
 import "./app.css"; // importa estilos globales
 
 ReactDOM.createRoot(document.getElementById("root")).render(
