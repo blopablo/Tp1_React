c022d2466f319f3b99c18510244aa3ed9a897e9c
blopablo <olbap0991@gmail.com>
2025-11-01 16:11:13 -0300

    Ya no se que hacer

--- Info-store/src/main.jsx
+++ Info-store/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app.jsx"; // importa el App verdadero
-import "./app.css"; // importa estilos globales
+import App from "./app.jsx"; 
+import "./app.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
